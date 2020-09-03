0f20502880434062c6f16a39a9ad758b21cc59f0
northantsvintage <northantsvintage@gmail.com>
2020-09-03 14:15:47 +0100

    netlify deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
