02017df1a18bc39e4818caf5ff9f29ffe1adad41
northantsvintage <northantsvintage@gmail.com>
2020-09-04 11:08:24 +0100

    app component structure and methods

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        title: "wake up",
+      },
+      {
+        id: 2,
+        title: "wash dishes",
+      },
+      {
+        id: 3,
+        title: "learn",
+      },
+    ],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+
+  clearList = () => {
+    console.log("clear list");
+  };
+
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
+  };
+
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+
+  render() {
+    console.log(this.state);
+    return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
